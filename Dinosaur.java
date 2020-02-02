94d5f5d0a6d57450da5bac0a7d3cfe3d38ff474f
Daniel <daniel.perez0628@gmail.com>
2020-02-01 21:16:46 -0600

    Created Dinosaur class get steps working

---
+++ Dinosaur.java
@@ -0,0 +1,13 @@
+public class Dinosaur {
+
+    private String name;
+    private Integer steps;
+    public Dinosaur (String name){
+        this.name = name;
+        this.steps = 0;
+    }
+
+    public Integer getSteps() {
+        return steps;
+    }
+}
